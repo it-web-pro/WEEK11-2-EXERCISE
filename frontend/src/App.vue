8bd1a352684020d710387085f138714db7a6cbaf
Bundit Thanasopon <bundittsp@gmail.com>
2021-03-26 22:35:53 +0700

    Init Code

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <!-- nav bar -->
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" @click="getBlogs">
+    <icon icon="address-book" size="lg" />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+// @ is an alias to /src
+export default {
+  name: 'Home',
+  data () {
+    return {
+      items: [],
+    }
+  },
+  methods:{
+    getBlogs () {
+      axios.get('http://localhost:3000/').then((response) => {
+        this.items = response.data
+      }).catch((err)=>{
+        console.log(err)
+      })
+    }
+  }
+}
+</script>
